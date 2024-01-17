3df19f954e53a756ff56fa97b9a4e140dc0b1b0f
Marcelo de Castro <marcelodecastro.ms@gmail.com>
2024-01-17 19:08:02 -0300

    Módulo 3 - Componentes do Design System

---
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -0,0 +1,27 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Heading, HeadingProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Heading',
+  component: Heading,
+  args: {
+    children: 'Custom Title',
+  },
+} as Meta<HeadingProps>
+
+export const Primary: StoryObj<HeadingProps> = {}
+
+export const CustomTag: StoryObj<HeadingProps> = {
+  args: {
+    children: 'H1 Heading',
+    as: 'h1',
+  },
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'Por padrão o Heading será `h2`, mas você pode alterar usando a tag `as`',
+      },
+    },
+  },
+}
---
+++ packages/react/src/components/MultiStep/index.tsx
@@ -0,0 +1,26 @@
+import { Label, MultiStepContainer, Step, Steps } from './styles'
+
+export interface MultiStepProps {
+  size: number
+  currentStep?: number
+}
+
+export function MultiStep({ size, currentStep = 1 }: MultiStepProps) {
+  return (
+    <MultiStepContainer>
+      <Label>
+        Passo {currentStep} de {size}
+      </Label>
+
+      <Steps css={{ '--steps-size': size }}>
+        {Array.from({ length: size }).map((_, i) => {
+          const stepNumber = i + 1
+
+          return (
+            <Step key={stepNumber} active={currentStep >= stepNumber}></Step>
+          )
+        })}
+      </Steps>
+    </MultiStepContainer>
+  )
+}

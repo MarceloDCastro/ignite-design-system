95913d6ca035f7fd81cbb93f9155d84a9af959c5
Marcelo de Castro <marcelodecastro.ms@gmail.com>
2024-01-17 20:17:07 -0300

    Módulo 4 - Melhorias no Storybook

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/MarceloDCastro.png',
     alt: 'Marcelo de Castro',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -7,6 +7,29 @@ export default {
   args: {
     children:
       'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus repudiandae rerum harum est et, facilis voluptate quae quasi a repellendus nam dicta itaque dolores? Sint quis perferendis tempora error reprehenderit!',
+    size: 'md',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
   },
 } as Meta<TextProps>
 
